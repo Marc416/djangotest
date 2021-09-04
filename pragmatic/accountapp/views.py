7b52c6773db382ddcc7fffde4035ca38d167d692
leejoonhee-quriously <joonhee@quriously.com>
2021-09-04 21:35:42 +0900

    Remove comment

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    #어떻게 여기서 새로 만든 accountapp 폴더를 추적해서 hello_world.html을 찾는가?
     return render(request,'accountapp/hello_world.html')
