7328d8242d84720d0f6a7fe7f59f13df6fd00c36
leejoonhee-quriously <joonhee@quriously.com>
2021-09-04 19:21:51 +0900

    Use extends, include

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request,'base.html')
+    #어떻게 여기서 새로 만든 accountapp 폴더를 추적해서 hello_world.html을 찾는가?
+    return render(request,'accountapp/hello_world.html')
