8e168e1f6bcab25229aa4e9bc6e57fe1ec7a45ce
Marc416 <juni416@naver.com>
2021-09-09 00:23:48 +0900

    Practice to Use DB

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -3,6 +3,8 @@ from django.urls import include, path
 import accountapp
 from accountapp.views import hello_world
 
+app_name = 'accountapp'
+
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -11,10 +12,20 @@ def hello_world(request):
         # hello_world_input이라는 이름을 가진 데이터를 가져와라
         input_data = request.POST.get('hello_world_input')
 
+        # DB인스턴스 생성
         new_hello_world = HelloWorld()
+        # Add data
         new_hello_world.text = input_data
+        # DB 저장
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # SQL SELECT ALL
+        hello_world_list = HelloWorld.objects.all()
+
+        # 재연결시(새로고침) Get으로 해당 페이지 보여줄 수 있도록
+        # 솔직히 reverse가 뭐 하는 건지모르겠음.
+        # 왜 String으로 변수들을 불러서 쓰는지도 모르겠음->이거 엄청 불편하지 않나?
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': 'It is Get'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
