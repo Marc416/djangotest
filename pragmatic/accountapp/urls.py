e3e7781d202f2bb6c75ce41e06bcb4d9d19245af
Marc416 <juni416@naver.com>
2021-09-11 10:56:45 +0900

    Create Account Create View

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 
 import accountapp
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
+    # 함수형 뷰를 이용할 때
+    # 어떤 패스를 받으면(http어쩌고..hello_world/), hello_world 함수를 실행시키렴,
+    # name = hello_world.html에 있는 것중에서!
     path('hello_world/', hello_world, name='hello_world'),
+    # 클래스형 뷰를 이용할 때
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -30,3 +33,14 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         # 렌더가 무슨 역할을 하는지 궁금하다.
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    # 어떤 모델을 쓸 것인가?
+    model = User
+    # 장고에서 제공해주는 가입 폼을 사용
+    form_class = UserCreationForm
+    # 계정만들기에 성공시 어디로 가게 할 것인가
+    # reverse_lazy는 클래스형 뷰 호출, reverse는 함수형 뷰 호출이라고만 알면 됨.
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
