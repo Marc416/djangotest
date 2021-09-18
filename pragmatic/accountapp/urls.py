f183f4e8de20239cb8b6d860cf0b742cf3097143
Marc416 <juni416@naver.com>
2021-09-18 22:47:49 +0900

    [26] Make account delete, Make Sign Up

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 
 import accountapp
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -22,4 +22,5 @@ urlpatterns = [
     # pk를 받겠다 근데 이 pk 는 int 이다
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
