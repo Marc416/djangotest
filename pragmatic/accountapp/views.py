6de29a8c8d90fc25ef47a8e83d198734ae3d2802
Marc416 <juni416@naver.com>
2021-09-20 15:30:36 +0900

    Check is signed user with login decorator from django method

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseForbidden
@@ -11,32 +12,30 @@ from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            # hello_world_input이라는 이름을 가진 데이터를 가져와라
-            input_data = request.POST.get('hello_world_input')
+    if request.method == "POST":
+        # hello_world_input이라는 이름을 가진 데이터를 가져와라
+        input_data = request.POST.get('hello_world_input')
 
-            # DB인스턴스 생성
-            new_hello_world = HelloWorld()
-            # Add data
-            new_hello_world.text = input_data
-            # DB 저장
-            new_hello_world.save()
+        # DB인스턴스 생성
+        new_hello_world = HelloWorld()
+        # Add data
+        new_hello_world.text = input_data
+        # DB 저장
+        new_hello_world.save()
 
-            # SQL SELECT ALL
-            hello_world_list = HelloWorld.objects.all()
+        # SQL SELECT ALL
+        hello_world_list = HelloWorld.objects.all()
 
-            # 재연결시(새로고침) Get으로 해당 페이지 보여줄 수 있도록
-            # 솔직히 reverse가 뭐 하는 건지모르겠음.
-            # 왜 String으로 변수들을 불러서 쓰는지도 모르겠음->이거 엄청 불편하지 않나?
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            # 렌더가 무슨 역할을 하는지 궁금하다.
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # 재연결시(새로고침) Get으로 해당 페이지 보여줄 수 있도록
+        # 솔직히 reverse가 뭐 하는 건지모르겠음.
+        # 왜 String으로 변수들을 불러서 쓰는지도 모르겠음->이거 엄청 불편하지 않나?
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        # 렌더가 무슨 역할을 하는지 궁금하다.
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
@@ -48,8 +47,6 @@ class AccountCreateView(CreateView):
     # reverse_lazy는 클래스형 뷰 호출, reverse는 함수형 뷰 호출이라고만 알면 됨.
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
-
-    
 
 
 class AccountDetailView(DetailView):
